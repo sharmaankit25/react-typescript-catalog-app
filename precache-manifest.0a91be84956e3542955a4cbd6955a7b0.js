0247462033ef0d03b5a7f563e02500ce0cb2cd9e
sharmaankit25 <sharmaankit25@users.noreply.github.com>
2020-08-01 13:51:44 +0000

    deploy: 8b2e5cdf05e93ce3fab61f3748f9e09d47dfc74e

---
+++ precache-manifest.0a91be84956e3542955a4cbd6955a7b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b684732d62602dfd8990cdbabf5a3de1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "008c16a4c2fcb6425855",
+    "url": "./static/css/main.ce194b8a.chunk.css"
+  },
+  {
+    "revision": "ea24cbd2f91471de0737",
+    "url": "./static/js/2.c544fe19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c544fe19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008c16a4c2fcb6425855",
+    "url": "./static/js/main.354b10e6.chunk.js"
+  },
+  {
+    "revision": "2ae72dc5cf8de5f5a47f",
+    "url": "./static/js/runtime-main.f1180e84.js"
+  }
+]);
