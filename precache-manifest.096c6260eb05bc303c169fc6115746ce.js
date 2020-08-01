9dd766ff292f5c3833e6f89dd0a88c230e86be28
sharmaankit25 <sharmaankit25@users.noreply.github.com>
2020-08-01 14:06:24 +0000

    deploy: 9b1a917402bde8508bf7ff6282edaa21c9639818

---
+++ precache-manifest.096c6260eb05bc303c169fc6115746ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9781d22e04c4444e6c87e9e19b67b62",
+    "url": "/react-typescript-catalog-app/index.html"
+  },
+  {
+    "revision": "bba99c249e89f970df2a",
+    "url": "/react-typescript-catalog-app/static/css/main.ce194b8a.chunk.css"
+  },
+  {
+    "revision": "ea24cbd2f91471de0737",
+    "url": "/react-typescript-catalog-app/static/js/2.c544fe19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-typescript-catalog-app/static/js/2.c544fe19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba99c249e89f970df2a",
+    "url": "/react-typescript-catalog-app/static/js/main.30d1c490.chunk.js"
+  },
+  {
+    "revision": "96480a98154e871284b9",
+    "url": "/react-typescript-catalog-app/static/js/runtime-main.ce482148.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0a91be84956e3542955a4cbd6955a7b0.js"
+  "/react-typescript-catalog-app/precache-manifest.096c6260eb05bc303c169fc6115746ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-typescript-catalog-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
