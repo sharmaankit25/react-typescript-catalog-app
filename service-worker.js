ae6a4fbeb457707c49bd8897a5ac1d1b9445cbc1
sharmaankit25 <sharmaankit25@users.noreply.github.com>
2020-08-01 13:27:21 +0000

    deploy: e3c62e0b8a4457e2d5bab4c6280a276042301245

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0a91be84956e3542955a4cbd6955a7b0.js"
+  "/react-typescript-catalog-app/precache-manifest.0ab710dbbd5cb9db65ac17f4d061c793.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-typescript-catalog-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
